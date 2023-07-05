19333ed29801323dd6ba463c7cfe18fda64fb1dc
Marvis Knight <knightmar78@gmail.com>
2023-07-05 11:20:17 -0700

    init commit

--- Fundamentals/p_fundamentals_1/Program.cs
+++ Fundamentals/p_fundamentals_1/Program.cs
@@ -1,22 +1,47 @@
-﻿ // For loop that INCLUDES 10
-// for(int i = 1; i <= 255; i++)
-// {
-//     Console.WriteLine(i);
-// }
+﻿    for(int i = 1; i <= 255; i++)
+    {
+        Console.WriteLine(i);
+    }
 
 
-    // Random rnd = new Random();
-    // for(int j = 0; j < 5; j++)
-    // {
-    //     Console.WriteLine(rnd.Next(10, 21));
-    // }
-    
     Random rnd = new Random();
-    int sum = new int();
     for(int j = 0; j < 5; j++)
     {
-        
-        Console.WriteLine("Trying to print rnd : {0}", rnd.Next(10, 21));
-        Console.WriteLine("Sum : {0}",sum + rnd.Next(10, 20));
+        Console.WriteLine(rnd.Next(10, 21));
+    }
+    int sum = 0;
+    Random Rnd = new Random();
+    for(int j = 0; j < 5; j++)
+    {
+        sum = sum + Rnd.Next(10, 21);
+    }
+    Console.WriteLine(sum);
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 3 == 0 && i % 5 == 0){
+            continue;
+        } else if ( i % 3 == 0 ^ i % 5 == 0){
+            Console.WriteLine(i);
+        }
+    }
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 3 == 0 && i % 5 == 0){
+            continue;
+        } else if ( i % 3 == 0){
+            Console.WriteLine("Fizz");
+        } else if ( i % 5 == 0){
+            Console.WriteLine("Buzz");
+        }
+    }
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 3 == 0 && i % 5 == 0){
+            Console.WriteLine("FizzBuzz");
+        } else if ( i % 3 == 0){
+            Console.WriteLine("Fizz");
+        } else if ( i % 5 == 0){
+            Console.WriteLine("Buzz");
+        }
     }
 
