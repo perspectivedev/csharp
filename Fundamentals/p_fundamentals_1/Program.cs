7be1d824bf2e9aefc94d049b996d8fecae10b51d
Marvis Knight <knightmar78@gmail.com>
2023-07-05 08:11:14 -0700

    practicing fundamentals

---
+++ Fundamentals/p_fundamentals_1/Program.cs
@@ -0,0 +1,22 @@
+﻿ // For loop that INCLUDES 10
+// for(int i = 1; i <= 255; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+    // Random rnd = new Random();
+    // for(int j = 0; j < 5; j++)
+    // {
+    //     Console.WriteLine(rnd.Next(10, 21));
+    // }
+    
+    Random rnd = new Random();
+    int sum = new int();
+    for(int j = 0; j < 5; j++)
+    {
+        
+        Console.WriteLine("Trying to print rnd : {0}", rnd.Next(10, 21));
+        Console.WriteLine("Sum : {0}",sum + rnd.Next(10, 20));
+    }
+
